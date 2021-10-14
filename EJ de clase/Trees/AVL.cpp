3c9fbcaa29138a682e567431e67002e42bab83df
unknown <ruy.guzman2002@gmail.com>
2021-10-13 21:33:02 -0500

    update

--- EJ de clase/Trees/AVL.cpp
+++ EJ de clase/Trees/AVL.cpp
@@ -97,60 +97,6 @@ template <class T> class BinarySearchTree {
             return minimum_element(current_node->left);
         }
 
-        Node<T>* deleteNode(Node<T> *current_node,int key_delete){
-
-            if(!current_node) return NULL;
-            // First search for the element to delete
-            else if(key_delete < current_node->key){
-                current_node->left = this->deleteNode(current_node->left, key_delete);
-            }
-            else if(key_delete > current_node->key){
-                current_node->right = this->deleteNode(current_node->right, key_delete);
-            }
-            else{
-                if(!current_node->left &&!current_node->right){
-                    // If the node is a leaf we just delete it
-                    delete current_node;
-                    current_node = NULL;
-                }
-                else if(!current_node->left){
-                    // If the node doesn't have left node, 
-                    // then just replace the current node with the right root
-                    Node<T> * right_node = current_node->right;
-                    delete current_node;
-                    current_node = right_node;
-                }
-                else if(!current_node->right){
-                    // If the node doesn't have right node, 
-                    // then just replace the current node with the left root
-                    Node<T> * left_node = current_node->left;
-                    delete current_node;
-                    current_node = left_node;   
-                }
-                else{
-                    // If the node to delete has both nodes
-                    // We search the minimum element in the right side
-                    Node<T>* successor  = this->minimum_element(current_node->right);
-                    // Replace the data with the minimum element (also the key)
-                    Node<T>* new_node_minimum = new Node<T>(
-                        successor ->data, successor->key, 
-                        current_node->left, current_node->right);
-
-                    if(current_node == this->root){
-                        this->root = new_node_minimum;
-                    }
-
-                    // Delete the current node
-                    delete current_node;
-
-                    // Now we recursively delete the minimum element in the right subtree
-                    current_node = new_node_minimum;
-                    current_node->right = this->deleteNode(current_node->right, successor->key);
-                }
-            }
-            return current_node;    
-        }
-
         int height(Node<T> *node){
             if(!node) return 0;
             int leftH = height(node->left);
@@ -207,23 +153,10 @@ int main() {
     a.insert(a.root,0,11);
     a.insert(a.root,0,20);
     a.insert(a.root,0,3);
-    a.insert(a.root,0,21);
-    a.insert(a.root,0,22);
     a.insert(a.root,0,1);
-    a.insert(a.root,0,5);
     a.insert(a.root,0,8);
-    a.insert(a.root,0,40);
-    a.insert(a.root,0,41);
-    a.insert(a.root,0,42);
-        a.insert(a.root,0,23);
-            a.insert(a.root,0,24);
 
-
-
-
-
-
-
+   
 
     a.inOrder(a.root);
     cout<<"\n";
