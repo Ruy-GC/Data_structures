285463bab6850913b00c7e7090f62d9370b42e44
unknown <ruy.guzman2002@gmail.com>
2021-10-16 22:41:34 -0500

    push working

--- EJ de clase/Trees/Max-heap.cpp
+++ EJ de clase/Trees/Max-heap.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 template <class T> class Node {
-    public:
-        T data;
-        int key;
+  public:
+      T data;
+      int key;
 
         Node(T new_data,int key){
             this->data = new_data;
@@ -24,61 +24,64 @@ template <class T> class MaxHeapTree {
         vector<Node<T>*> tree;
         
     public:
-        MaxHeapTree(T new_data,int key){
-            Node<T> *root = new Node<T>(new_data,key);
-            tree.push_back(root);
+        MaxHeapTree(){
+
         }
 
-        void insert(T data, int key){
-          Node<T> *new_node = new Node<T>(data,key);
-          tree.push_back(new_node);
+        void push(T data, int key){
+            Node<T> *new_node = new Node<T>(data,key);
+            tree.push_back(new_node);
 
-          int index = tree.size()-1;
+            int i = tree.size()-1;
 
-          while(index >= 0){
-            
-            int parent = floor((index-1)/2);
-            if (tree[index] > tree[parent]){
-              cout << "son:" << index << endl;
-              cout << "parent: " << parent << endl;
-              swap(tree,index,parent); 
+            while (i != 0 && tree[parent(i)]->key < tree[i]->key){
+                swap( tree ,i, parent(i));
+                i = parent(i);
             }
-            index--;
-          }
-          //if(index != 0){
-            
-          //}
-        }
-
-
-        void swap(vector<Node<T>*>&tree,int i1,int i2){
-          Node<T> *temp = tree[i1];
-          tree[i1] = tree[i2];
-          tree[i2] = temp;
         }
         
         void printTree(){
-          for(int i = 0; i < tree.size(); i++){
-            cout<<tree[i]->key<<" ";
-          }
-          cout<<endl;
+            for(int i = 0; i < tree.size(); i++){
+              cout<<tree[i]->key<<" ";
+            }
+            cout<<endl;
         }
-        /*
+
+        void swap(vector<Node<T>*>&tree,int i1,int i2){
+            Node<T> *temp = tree[i1];
+            tree[i1] = tree[i2];
+            tree[i2] = temp;
+        }
+        
+        bool empty(){
+          return tree.size() == 0;
+        }
+
+        int size(){
+          return tree.size();
+        }
+
+        Node<T> * top(){
+          return tree[0];
+        }
+
         int parent(int i) { return (i-1)/2; }
         
         // to get index of left child of node at index i
         int left(int i) { return (2*i + 1); }
   
         // to get index of right child of node at index i
-        int right(int i) { return (2*i + 2); }*/
+        int right(int i) { return (2*i + 2); }
 };
 
 int main() {
-  MaxHeapTree<int> tree(1,20);
-  tree.insert(1,30);
-  tree.printTree();
-  cout << endl;
-  tree.insert(2,50); 
-  tree.insert(2,10);         
-  tree.printTree();
+    MaxHeapTree<int> tree;
+    tree.push(2,30);
+    tree.push(2,50); 
+    tree.push(2,10); 
+    tree.push(2,20);         
+    tree.push(2,25);         
+    tree.push(2,11);         
+    tree.push(2,12);      
+    tree.printTree();
 }
