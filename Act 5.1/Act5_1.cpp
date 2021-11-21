9b129a3b8d35fba8a72193b8ea13a9f7ad348cb5
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2021-11-21 12:39:36 -0600

    functional program

--- Act 5.1/Act5_1.cpp
+++ Act 5.1/Act5_1.cpp
@@ -1,10 +1,16 @@
+/*
+Programa que implementa una hash table con manejo de colisiones por encadenamiento 
+y por prueba cuadrática
+
+Autor: Ruy Guzmán Camacho A01639912
+Entrega: 22/11/2021
+*/
+
 #include <iostream>
 #include <list>
-#include <type_traits>
 #include <string>
 #include <vector>
 #include <utility>
-#include <math.h>
 using namespace std;
 
 template <class T> class Hash {
@@ -22,11 +28,15 @@ template <class T> class Hash {
 
         //Mejor: O(1) si no hay colisión
         //Promedio: O(log n) en caso de colisión,  n siendo los espacios de la tabla
-        //Peor: O(n) 
+        //Peor: O(n) en caso de que no se pueda insertar el elemento en la tabla o que tenga iterar n(tamaño de la tabla) veces para ubicarlo
         void quadratic(int key, T item){
             int H = hashFunction(key);
             for(int j = 0; j<qbuckets;j++){
                 int index = (H + (j*j)) % qbuckets;
+                /*if(index > qbuckets-1){
+
+                }*/
+
                 if(quadHashTable[index].first == -1 || quadHashTable[index].first == key){
                     quadHashTable[index] = make_pair(key,item);
                     fail = false;
@@ -34,11 +44,11 @@ template <class T> class Hash {
                 }
                 fail = true;
             }
-            if(fail) resizeQuad();  
+            //if(fail) resizeQuad();  
         }
 
         //Mejor: O(1) si no hay colisión
-        //Promedio: O(L) en caso de colisión, donde L es la longitud de la lista ligada en el espacio de la tabla
+        //Promedio: O(L) en caso de colisión, donde L es la longitud de la lista ligada en indice obtenido por el hashing
         void chain(int key, T item){
             pair<int,T> new_element (key,item);
             int index = hashFunction(key);
@@ -53,14 +63,24 @@ template <class T> class Hash {
             chainHashTable[index].push_back(new_element);
         }
 
-        //función auxiliar de la prueba cuadrática, amplía la tabla hash
-        void resizeQuad(){
-            this->qbuckets *= 2;
+        //función auxiliar de la prueba cuadrática, amplía la tabla hash y devuelve los valores que ya estaban dentor para su reubicación
+        vector<pair<int,T>> resizeQuad(){
+            vector<pair<int,T>> items;
+            for(int i = 0; i< qbuckets; i++){
+                if(quadHashTable[i].first != -1 ){
+                    items.push_back(quadHashTable[i]);
+                }
+            }
+
+            this->qbuckets = (qbuckets * 2) + 1;
+
             quadHashTable.clear();
-             for(int i = 0; i < qbuckets; i++){
+            for(int i = 0; i < qbuckets; i++){
                 pair<int,T> new_item (-1,NULL);
                 quadHashTable.push_back(new_item);
             }
+
+            return items;
         }
 
     public:
@@ -85,14 +105,20 @@ template <class T> class Hash {
             }
         }
 
-        //Mejor: O(n) siendo n la cantidad de  items a agregar
-        //Peor: O(n^2) siendo n la cantidad de  items a agregar si se tiene que redimensionar la tabla
-        //función de prueba cuadratica para el usuario 
+        //O(n) siendo n la cantidad de  items a agregar
+        //En caso de que se tenga redimensional la tabla n se vuelve (Nuevos items + items en la tabla)
         void quadratic(vector<pair<int,T>> items){
             int i = 0;
             while (i < items.size()){
                 this->quadratic(items[i].first,items[i].second);
                 if(fail){
+
+                    vector<pair<int,T>> new_items;
+                    vector<pair<int,T>> prev_items = resizeQuad();
+                    new_items.reserve(items.size() + prev_items.size());
+                    new_items.insert(new_items.end(),prev_items.begin(),prev_items.end());
+                    new_items.insert(new_items.end(),items.begin(),items.end());
+                    items = new_items;
                     i = 0;
                 }else{
                     i++;
@@ -124,18 +150,40 @@ template <class T> class Hash {
 };
 
 int main(){
-    Hash<int> Test (4);
-    vector<pair<int,int>> items;
-    items.push_back(make_pair(10,25));
-    items.push_back(make_pair(15,26));
-    items.push_back(make_pair(5,27));
-    items.push_back(make_pair(13,1));
-    items.push_back(make_pair(18,2));
-    items.push_back(make_pair(8,3));
+    for(int i = 0; i < 4; i++){
+        int buckets = 0;
+        int key = 0;
+        int item = 1;
+        cout<<"Ingrese la antidad de espacios de la hash Table:";
+        cin>>buckets;
 
-    Test.chain(items);
-    Test.quadratic(items);
-    
-    Test.printChain();
-    Test.printQuad();
+        Hash<int> Table(buckets);
+        vector<pair<int,int>> items;
+        cout<<"\nIngrese las llaves a agregar en la tabla (-1 para terminar)\n";
+
+        while (key != -1){
+            cout<<"Llave "<<item<<": ";
+            cin>>key;
+
+            if(key != -1){
+                items.push_back(make_pair(key,item));
+            }
+                
+            item++;
+        }
+
+        cout<<"\nTabla generada con manejo de colisiones por encadenamiento: \n";
+        Table.chain(items);
+        Table.printChain();
+
+        cout<<"\nTabla generada con manejo de colisiones por prueba cuadratica: \n";
+        cout<<"*Si se ingresaron más elememtos que espacios en la tabla habrá un redimensionamiento*\n";
+        Table.quadratic(items);
+        Table.printQuad();
+        cout<<"\n";
+
+        system("pause");
+    }
+
+    return 0;
 }
